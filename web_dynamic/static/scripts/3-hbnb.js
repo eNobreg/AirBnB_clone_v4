7acc34c0f34fececa677280f6e9cf3c75137d7ef
Anthony <huggins9000211@gmail.com>
2020-05-16 16:55:13 -0400

    test

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
     }
   });
   // There's a shorthand way to do it with $.post but I can't figure it out
-  //  
+  //
   // $.post('http://localhost:5001/api/v1/places_search/', '{}', function (data) {
   //   console.log(data)
   // }, "json");
@@ -38,26 +38,28 @@ $(document).ready(function () {
     contentType: 'application/json',
     dataType: 'json',
     success: function (data) {
+      let entry;
       for (entry of data) {
         // Ternary operators to insert correct plural
-        const bathroom = (entry.number_bathrooms > 1) ? ' Bathrooms</div>' : ' Bathroom</div>'
-        const guests = (entry.max_guest > 1) ? ' Guests</div>' : ' Guest</div>'
-        const rooms = (entry.number_rooms > 1) ? ' Bedrooms</div>' : ' Bedroom</div>'
+        const bathroom = (entry.number_bathrooms > 1) ? ' Bathrooms</div>' : ' Bathroom</div>';
+        const guests = (entry.max_guest > 1) ? ' Guests</div>' : ' Guest</div>';
+        const rooms = (entry.number_rooms > 1) ? ' Bedrooms</div>' : ' Bedroom</div>';
 
         // Create html each loop
         const html = '<article>' +
-                     '<div class="title_box">'+
+                     '<div class="title_box">' +
                      '<h2>' + entry.name + '</h2>' +
-                     '<div class="price_by_night">$' + entry.price_by_night + '</div>'+
-                     '</div>'+
-                     '<div class="information">'+
-                     '<div class="max_guest">' + entry.max_guest + guests+
-                     '<div class="number_rooms">' + entry.number_rooms + rooms+
-                     '<div class="number_bathrooms">' + entry.number_bathrooms + bathroom+
-                     '</div>'+
-                     '<div class="description">'+ entry.description + '</div>' + '</article>'
+                     '<div class="price_by_night">$' + entry.price_by_night + '</div>' +
+                     '</div>' +
+                     '<div class="information">' +
+                     '<div class="max_guest">' + entry.max_guest + guests +
+                     '<div class="number_rooms">' + entry.number_rooms + rooms +
+                     '<div class="number_bathrooms">' + entry.number_bathrooms + bathroom +
+                     '</div>' +
+                     '<div class="description">' + entry.description + '</div>' + '</article>';
         // Append the HTML to the correct section
-        $('section.places').append(html)
+        $('section.places').append(html);
       }
-  }});
+    }
+ });
 });
